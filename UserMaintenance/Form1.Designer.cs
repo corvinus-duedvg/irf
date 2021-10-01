b72531ecc6ab84ee23de402c28ba4af27e25c9df
Gábor Tamás Mozol <gabor.mozol@stud.uni-corvinus.hu>
2021-10-01 10:59:11 +0200

    t/c v2

--- UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/Form1.Designer.cs
@@ -33,6 +33,7 @@ namespace UserMaintenance
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -40,13 +41,13 @@ namespace UserMaintenance
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(13, 13);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(212, 238);
+            this.listBox1.Size = new System.Drawing.Size(173, 238);
             this.listBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(231, 15);
+            this.label1.Location = new System.Drawing.Point(192, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
@@ -61,7 +62,7 @@ namespace UserMaintenance
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(234, 199);
+            this.button1.Location = new System.Drawing.Point(234, 143);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(138, 50);
             this.button1.TabIndex = 5;
@@ -69,11 +70,22 @@ namespace UserMaintenance
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(234, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 50);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -91,6 +103,7 @@ namespace UserMaintenance
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,11 +15,13 @@ namespace UserMaintenance
     public partial class Form1 : Form
     {
         BindingList<User> users = new BindingList<User>();
+        SaveFileDialog sfd1 = new SaveFileDialog();
         public Form1()
         {
             InitializeComponent();
             label1.Text = ResourceDefault.FullName;
             button1.Text = ResourceDefault.Add;
+            button2.Text = ResourceDefault.Write;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -32,6 +35,22 @@ namespace UserMaintenance
                 FullName = textBox1.Text,
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sfd1.Filter= "CSV file (*.csv)|*.csv| All Files (*.*)|*.*";
+            if (sfd1.ShowDialog()==DialogResult.OK)
+            {
+                StreamWriter writer = new StreamWriter(sfd1.Filter);
+                writer.WriteLine("FullName");
+
+                foreach (User user in users)
+                {
+                    writer.Write(user.FullName);
+                }
+                writer.Close();
+            }
         }
     }
 }
