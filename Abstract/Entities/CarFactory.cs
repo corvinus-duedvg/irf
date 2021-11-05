d86eb3011d9bcc75752ef039c17d1a94cfa3ef70
Gábor Tamás Mozol <gabor.mozol@stud.uni-corvinus.hu>
2021-11-05 17:10:13 +0100

    Abstract facotry

--- Abstract/Entities/CarFactory.cs
+++ Abstract/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace Abstract.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
