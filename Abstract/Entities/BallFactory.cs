cf1004c95da813e109465c033388365889e77252
Gábor Tamás Mozol <gabor.mozol@stud.uni-corvinus.hu>
2021-11-05 16:45:19 +0100

    Ballfactory

---
+++ Abstract/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract.Entities
+{
+    public class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
