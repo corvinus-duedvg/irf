2c5b1dee317d361ac666df415ad6cc27eb5f0378
Gábor Tamás Mozol <gabor.mozol@stud.uni-corvinus.hu>
2021-11-05 17:41:34 +0100

    Szín

--- Abstract/Form1.Designer.cs
+++ Abstract/Form1.Designer.cs
@@ -33,13 +33,17 @@ namespace Abstract
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnSlectCar = new System.Windows.Forms.Button();
+            this.btnSelectBall = new System.Windows.Forms.Button();
+            this.lblNext = new System.Windows.Forms.Label();
+            this.btnColor = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
-            this.mainPanel.Location = new System.Drawing.Point(13, 13);
+            this.mainPanel.Location = new System.Drawing.Point(12, 89);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(759, 386);
+            this.mainPanel.Size = new System.Drawing.Size(760, 310);
             this.mainPanel.TabIndex = 0;
             // 
             // createTimer
@@ -54,15 +58,59 @@ namespace Abstract
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // btnSlectCar
+            // 
+            this.btnSlectCar.Location = new System.Drawing.Point(12, 12);
+            this.btnSlectCar.Name = "btnSlectCar";
+            this.btnSlectCar.Size = new System.Drawing.Size(75, 23);
+            this.btnSlectCar.TabIndex = 1;
+            this.btnSlectCar.Text = "CAR";
+            this.btnSlectCar.UseVisualStyleBackColor = true;
+            this.btnSlectCar.Click += new System.EventHandler(this.btnSelectCar_Click);
+            // 
+            // btnSelectBall
+            // 
+            this.btnSelectBall.Location = new System.Drawing.Point(93, 12);
+            this.btnSelectBall.Name = "btnSelectBall";
+            this.btnSelectBall.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectBall.TabIndex = 2;
+            this.btnSelectBall.Text = "BALL";
+            this.btnSelectBall.UseVisualStyleBackColor = true;
+            this.btnSelectBall.Click += new System.EventHandler(this.btnSelectBall_Click);
+            // 
+            // lblNext
+            // 
+            this.lblNext.AutoSize = true;
+            this.lblNext.Location = new System.Drawing.Point(273, 17);
+            this.lblNext.Name = "lblNext";
+            this.lblNext.Size = new System.Drawing.Size(70, 13);
+            this.lblNext.TabIndex = 3;
+            this.lblNext.Text = "Coming Next:";
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Fuchsia;
+            this.btnColor.Location = new System.Drawing.Point(93, 41);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 4;
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.lblNext);
+            this.Controls.Add(this.btnSelectBall);
+            this.Controls.Add(this.btnSlectCar);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +119,10 @@ namespace Abstract
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button btnSlectCar;
+        private System.Windows.Forms.Button btnSelectBall;
+        private System.Windows.Forms.Label lblNext;
+        private System.Windows.Forms.Button btnColor;
     }
 }
 
--- Abstract/Form1.cs
+++ Abstract/Form1.cs
@@ -1,4 +1,5 @@
-﻿using Abstract.Entities;
+﻿using Abstract.Abstractions;
+using Abstract.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,13 +14,18 @@ namespace Abstract
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
+        private List<Toy> _toys = new List<Toy>();
+        private Toy _nextToy;
 
-        private BallFactory _factory;
-        public BallFactory Factory
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set
+            {
+                _factory = value;
+                DisplayNext();
+            }
         }
         public Form1()
         {
@@ -28,28 +34,59 @@ namespace Abstract
         }
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            _balls.Add(ball);
-            ball.Left = -ball.Width;
-            mainPanel.Controls.Add(ball);
+            var toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var toy in _toys)
             {
-                ball.MoveToy();
-                if (ball.Left > maxPosition)
-                    maxPosition = ball.Left;
+                toy.MoveToy();
+                if (toy.Left > maxPosition)
+                    maxPosition = toy.Left;
             }
 
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
-                mainPanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                var oldestToy = _toys[0];
+                mainPanel.Controls.Remove(oldestToy);
+                _toys.Remove(oldestToy);
             }
+        }
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = lblNext.Top;
+            _nextToy.Left = lblNext.Left+ lblNext.Width+20;
+            Controls.Add(_nextToy);
+        }
+        private void btnSelectCar_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void btnSelectBall_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory
+            {
+                BallColor = btnColor.BackColor
+            };
+        }
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
         }
     }
 }
